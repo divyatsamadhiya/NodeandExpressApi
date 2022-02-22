9e1d3106bee5fdfa05a54c6131b45b9b23832a97
divyatsamadhiya <sdivyatuniverse@gmail.com>
2022-02-22 19:01:05 +0530

    update method changed

--- controller/route_controller.js
+++ controller/route_controller.js
@@ -3,7 +3,7 @@ const Product = require("../productSchema");
 //callback function approach for talking with db
 const getProducts = (req, res) => {
     Product.find({}, function (err, product) {
-        if (err) console.err(err);
+        if (err) res.status(500).json({ msg: err });
         res.status(200).json({ product });
     });
 };
@@ -22,24 +22,40 @@ const getProducts = (req, res) => {
 const createProduct = (req, res) => {
     const product = new Product(req.body);
     product.save(function (err, product) {
-        if (err) return console.err(err);
+        if (err) res.status(500).json({ msg: err });
         res.status(201).json({ product });
     });
 };
 
-const getProductByName = (req, res) => {
-    Product.findOne({ name: req.params.productName }, function (err, product) {
-        if (err) return console.err(err);
+// const getProductByName = (req, res) => {
+//     Product.findOne({ name: req.params.productName }, function (err, product) {
+//         if (err) res.status(500).json({ msg: err });
+//         res.status(200).json({ product });
+//     });
+// };
+
+//method with product's existence in db validation
+const getProductByName = async (req, res) => {
+    try {
+        const product = await Product.findOne({ name: req.params.productName });
+        if (!product) {
+            res.status(404).json({
+                msg: `No product exist with name ${req.params.productName}`,
+            });
+        }
         res.status(200).json({ product });
-    });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 };
 
 const updateProduct = (req, res) => {
     Product.findOneAndUpdate(
         { name: req.params.productName },
         req.body,
+        { new: true, runValidators: true },
         function (err, updatedProduct) {
-            if (err) return console.err(err);
+            if (err) res.status(500).json({ msg: err });
             res.status(201).json({ updatedProduct });
         }
     );
@@ -47,7 +63,7 @@ const updateProduct = (req, res) => {
 
 const deleteProduct = (req, res) => {
     Product.deleteOne({ name: req.params.productName }, function (err, data) {
-        if (err) return console.err(err);
+        if (err) res.status(500).json({ msg: err });
         res.status(204).json({ data });
     });
 };
